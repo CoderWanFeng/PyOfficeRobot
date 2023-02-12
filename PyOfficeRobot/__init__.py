de3d8018f5e7ffbdbb1d8e093619c993f374c646
CoderWanFeng <875143930@qq.com>
2023-02-12 17:25:34 +0800

    publish 0.0.15 : 增加chatGPT版本https://blog.51cto.com/u_15493782/6044962

--- PyOfficeRobot/__init__.py
+++ PyOfficeRobot/__init__.py
@@ -3,11 +3,8 @@ from PyOfficeRobot.api import file
 from PyOfficeRobot.lib.CONST import SPLIT_LINE
 
 print(SPLIT_LINE)
-print('【PyOfficeRobot库】，功能持续更新中')
-print('使用有问题 or 提交你的功能需求 or 参与项目开发')
-print('1、全部功能【视频 & 文字】教程：https://www.python-office.com/office/robot.html')
-print('2、请+【项目交流群】：http://t.cn/A65MiFvH')
-print('3、本开源项目的【代码仓库】：https://github.com/CoderWanFeng/PyOfficeRobot')
-print('4、【常见问题】：https://www.python-office.com/ref/qaq.html')
-print('5、【ChatGPT版本】：https://blog.51cto.com/u_15493782/6044962')
+print('【PyOfficeRobot，微信机器人】')
+print('全部功能【视频教程：https://www.python-office.com/office/robot.html')
+print('本开源项目的【代码仓库】：https://github.com/CoderWanFeng/PyOfficeRobot')
+print('【ChatGPT版本】：https://mp.weixin.qq.com/s/HJfLZILUOWn4TK8qk3DL9w')
 print(SPLIT_LINE)
