559b5570f13cbb0c3bfdfbe53006675613365403
CoderWanFeng <875143930@qq.com>
2023-03-19 20:09:17 +0800

    publish 0.1.2 ： 接入ChatGPT👉https://blog.51cto.com/u_15493782/6131326

--- PyOfficeRobot/lib/decorator_utils/instruction_url.py
+++ PyOfficeRobot/lib/decorator_utils/instruction_url.py
@@ -20,7 +20,7 @@ chat_dict = {"chat_by_keywords": "https://www.bilibili.com/video/BV1fV4y1M7ju",
                "receive_message": "",
                "send_message": "https://www.bilibili.com/video/BV1Jt4y1j7F1",
                "send_message_by_time": "https://www.bilibili.com/video/BV1m8411b7LZ",
-               "chat_by_gpt": "https://www.bilibili.com/video/BV1m8411b7LZ",
+               "chat_by_gpt": "https://blog.51cto.com/u_15493782/6131326",
 }
 file_dict = {
     "send_file": "https://www.bilibili.com/video/BV1te4y1y7Ro",
