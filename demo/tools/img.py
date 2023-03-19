6dc8dce0aae5be6d3200d93228d5c9c0607f4598
CoderWanFeng <875143930@qq.com>
2023-03-19 21:36:35 +0800

    update README.md

---
+++ demo/tools/img.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@WeChat     ：CoderWanFeng
+@Blog      ：www.python-office.com
+@Date    ：2023/3/19 21:04 
+@Description     ：
+'''
+import office
+
+office.image.add_watermark(file=r'./imgs/公众号+Web.jpg', mark='程序员晚枫')
