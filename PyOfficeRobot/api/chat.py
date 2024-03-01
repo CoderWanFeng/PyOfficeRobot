9b8d4f14d97dd9bdb91e8907adaa9f8a6c3904b9
CoderWanFeng <1957875073@qq.com>
2024-03-02 00:15:21 +0800

    发布 0.1.20：加入智谱AI大模型

--- PyOfficeRobot/api/chat.py
+++ PyOfficeRobot/api/chat.py
@@ -148,3 +148,24 @@ def chat_ali(who, key):
                 wx.SendMsg(reply_msg, who)  # 向`who`发送消息
         except:
             pass
+
+
+def chat_by_zhipu(who, key, model='glm-4'):
+    wx.GetSessionList()  # 获取会话列表
+    wx.ChatWith(who)  # 打开`who`聊天窗口
+    temp_msg = None
+    while True:
+        try:
+            friend_name, receive_msg = wx.GetAllMessage[-1][0], wx.GetAllMessage[-1][1]  # 获取朋友的名字、发送的信息
+            if (friend_name == who) & (receive_msg != temp_msg):
+                """
+                条件：
+                朋友名字正确:(friend_name == who)
+                不是上次的对话:(receive_msg != temp_msg)
+                对方内容在自己的预设里:(receive_msg in kv.keys())
+                """
+                print(f'【{who}】发送：【{receive_msg}】')
+                reply_msg = poai.chat.zhipu(key, prompt=receive_msg, model=model)
+                wx.SendMsg(reply_msg, who)  # 向`who`发送消息
+        except:
+            pass
--- tests/chat.py
+++ tests/chat.py
@@ -7,3 +7,6 @@ class TestFile(unittest.TestCase):
 
     def test_chat_ali(self):
         chat_ali(who='每天进步一点点', key=os.getenv('TY_KEY'))
+
+    def test_chat_zhipu(self):
+        chat_by_zhipu(who='每天进步一点点', key='e29c16C0HFKSpNHzIm')
